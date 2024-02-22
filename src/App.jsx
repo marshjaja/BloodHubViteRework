de80521a6e0d319398f9cb1f2bdab27865297da8
marshjaja <114920895+marshjaja@users.noreply.github.com>
2024-02-22 20:06:35 +0000

    add: Add updated App and Main files

--- src/App.jsx
+++ src/App.jsx
@@ -4,36 +4,35 @@ import {
 	BrowserRouter as Router,
 	Route,
 	Routes,
-	NavLink,
-	Link,
+	useLocation,
 } from "react-router-dom";
-
 import Home from "./pages/Home";
 import CanIGiveBlood from "./pages/CanIGiveBlood";
 import LandingScreen from "./pages/LandingScreen";
+import NavBar from "./components/NavBar";
 
 function App() {
 	return (
+		<Router>
+			<AppContent />
+		</Router>
+	);
+}
+
+function AppContent() {
+	const location = useLocation();
+	const isNotLandingPage = location.pathname !== "/";
+
+	return (
 		<>
-			<Router>
-				<nav>
-					<ul>
-						<li>
-							<Link to="/home">Home</Link>
-						</li>
-						<li>
-							<Link to="/can-i-give-blood">Can I Give Blood</Link>
-						</li>
-					</ul>
-				</nav>
-				<main>
-					<Routes>
-						<Route path="/" element={<LandingScreen />} />
-						<Route path="/home" element={<Home />} />
-						<Route path="/can-i-give-blood" element={<CanIGiveBlood />} />
-					</Routes>
-				</main>
-			</Router>
+			{isNotLandingPage && <NavBar />}
+			<main className={isNotLandingPage ? "main-container" : ""}>
+				<Routes>
+					<Route path="/" element={<LandingScreen />} />
+					<Route path="/home" element={<Home />} />
+					<Route path="/can-i-give-blood" element={<CanIGiveBlood />} />
+				</Routes>
+			</main>
 		</>
 	);
 }
