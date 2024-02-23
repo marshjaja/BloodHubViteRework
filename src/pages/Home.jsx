134645141f5a3585de4af817a64ac003560df082
marshjaja <114920895+marshjaja@users.noreply.github.com>
2024-02-23 01:35:51 +0000

    feat:Add updated home component files

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import { Image, Form, Button, InputGroup, FormControl } from "react-bootstrap";
+import Image1 from "../img/image1.png";
+import "../styles/Home.styles.css";
 
 function Home() {
 	return (
 		<>
-			<div>Home</div>
+			<div className="home-background">
+				<div className="content-container">
+					<InputGroup size="lg" className="custom-input-group">
+						{" "}
+						{/* Custom class for max-width */}
+						<FormControl
+							aria-label="Large"
+							aria-describedby="inputGroup-sizing-sm"
+							className="custom-form-control"
+						/>
+						<Button type="submit" className="btn btn-primary">
+							Search
+						</Button>
+					</InputGroup>
+				</div>
+			</div>
 		</>
 	);
 }
