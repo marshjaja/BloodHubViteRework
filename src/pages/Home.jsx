81508182f1796ecf5ccdfe2567f78c9c31a5b12d
marshjaja <114920895+marshjaja@users.noreply.github.com>
2024-02-29 13:26:41 +0000

    add: Update styles for the Home  component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -28,36 +28,47 @@ function Home() {
 		<>
 			<div className="home-background">
 				<div className="content-container">
-					<div className="row">
-						<div className="col-6 col-lg-3 d-flex justify-content-center">
-							{/* <InputGroup size="lg" className="custom-input-group">
-						<FormControl
-							aria-label="Large"
-							aria-describedby="inputGroup-sizing-sm"
-							className="custom-form-control"
-						/> */}
-							<h1>Hello</h1>
-						</div>
-						{/* <Button type="submit" className="btn custom-btn">
+					{/* <Button type="submit" className="btn custom-btn">
 						Search
 					</Button> */}
-						<div className="col-6 col-lg-3 d-flex justify-content-center"></div>
-						<Button as={Link} className="" to="/where-to-donate">
-							Where to Donate
-						</Button>
-						{/* </InputGroup> */}
+					<div className="row home-text">
+						<div className="col-12 d-flex justify-content-center">
+							<p>
+								Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut
+								sequi ducimus consequatur, voluptate tenetur aliquid quasi,
+								reprehenderit blanditiis dolor atque doloremque praesentium
+								incidunt ut fuga veniam? Quo expedita deserunt facere nam omnis
+								quisquam neque incidunt asperiores voluptas, non dolore magni,
+								unde odio cum, voluptatibus harum numquam quibusdam provident!
+								Consectetur, explicabo.
+							</p>
+						</div>
+						<div className="col-12 d-flex justify-content-center">
+							<Button as={Link} className="home-btn " to="/where-to-donate">
+								Where to Donate
+							</Button>
+						</div>
 					</div>
 				</div>
 			</div>
+
+			{/* </div> */}
 			<div className="container text-center">
 				<div className="row">
 					<div className="col-6 col-lg-3 d-flex justify-content-center">
-						<div className="circle">
-							<FontAwesomeIcon
-								icon={faHeartPulse}
-								size="5x"
-								style={{ color: "rgb(185, 26, 26)" }}
-							/>
+						<div className="d-flex flex-column align-items-center">
+							<div className="circle">
+								<FontAwesomeIcon
+									icon={faHeartPulse}
+									size="5x"
+									style={{ color: "rgb(185, 26, 26)" }}
+								/>
+							</div>
+							<h4 style={{ marginTop: "10px" }}>Give blood</h4>{" "}
+							<p>
+								Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+								Doloremque, voluptas?
+							</p>{" "}
 						</div>
 					</div>
 					<div className="col-6 col-lg-3 d-flex justify-content-center">
@@ -69,28 +80,44 @@ function Home() {
 									style={{ color: "rgb(185, 26, 26)" }}
 								/>
 							</div>
-							<p style={{ marginTop: "10px" }}>
-								kbdfbdkasbfksdbkj vkjwfbowbofbouwbfgouwbfbwoubfoweb
+							<h4 style={{ marginTop: "10px" }}>Where to donate</h4>{" "}
+							<p>
+								Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+								Doloremque, voluptas?
 							</p>{" "}
 						</div>
 					</div>
 					<div className="col-6 col-lg-3 d-flex justify-content-center">
-						<div className="circle">
-							<FontAwesomeIcon
-								icon={faDroplet}
-								size="5x"
-								style={{ color: "rgb(185, 26, 26)" }}
-							/>
+						<div className="d-flex flex-column align-items-center">
+							<div className="circle">
+								<FontAwesomeIcon
+									icon={faDroplet}
+									size="5x"
+									style={{ color: "rgb(185, 26, 26)" }}
+								/>
+							</div>
+							<h4 style={{ marginTop: "10px" }}>Get to know the blood types</h4>{" "}
+							<p>
+								Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+								Doloremque, voluptas?
+							</p>{" "}
 						</div>
 					</div>
 					<div className="col-6 col-lg-3 d-flex justify-content-center">
-						<div className="circle">
-							<FontAwesomeIcon
-								className="icon"
-								icon={faArrowsSpin}
-								size="5x"
-								style={{ color: "rgb(185, 26, 26)" }}
-							/>
+						<div className="d-flex flex-column align-items-center">
+							<div className="circle">
+								<FontAwesomeIcon
+									className="icon"
+									icon={faArrowsSpin}
+									size="5x"
+									style={{ color: "rgb(185, 26, 26)" }}
+								/>
+							</div>
+							<h4 style={{ marginTop: "10px" }}>Blood donation process</h4>{" "}
+							<p>
+								Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+								Doloremque, voluptas?
+							</p>{" "}
 						</div>
 					</div>
 				</div>
